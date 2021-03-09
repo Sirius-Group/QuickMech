a3c4885cd036e6f24272d808c33f454f32fe01dd
abuzahra98 <dawoodbasher@gmail.com>
2021-03-09 22:20:38 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -1,26 +1,19 @@
+/* eslint-disable no-unused-vars */
 'use strict';
-
 const Service = function (name, location, service, time) {
-
   this.name = name;
   this.location = location;
   this.service = service;
   this.time = time;
   Service.allService.push(this);
-
 };
-
 Service.allService = [];
 
-
 function populateForm() {
-
   let serviceArray = ['Car care', 'Cleaning', 'Electrical', 'Mechanical', 'Plumbing', 'Painting', 'Gardening', 'CCTV service'];
-
-  let timeArray =
-    ['01:00 AM', '02:00 AM', '03:00 AM', '04:00 AM', '05:00 AM', '06:00 AM', '07:00 AM', '08:00 AM', '09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM',
-      '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM', '06:00 PM', '07:00 PM', '08:00 PM', '09:00 PM', '10:00 PM', '11:00 PM', '12:00 AM'];
-
+  let timeArray = ['01:00 AM', '02:00 AM', '03:00 AM', '04:00 AM', '05:00 AM', '06:00 AM', '07:00 AM', '08:00 AM', '09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM',
+    '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM', '06:00 PM', '07:00 PM', '08:00 PM', '09:00 PM', '10:00 PM', '11:00 PM', '12:00 AM'];
+  // this line to push array in list
   const selectService = document.getElementById('service');
   const selectTime = document.getElementById('serviceTime');
 
@@ -28,18 +21,14 @@ function populateForm() {
     const ServiceOption = document.createElement('option');
     selectService.appendChild(ServiceOption);
     ServiceOption.textContent = `${serviceArray[i]}`;
-
   }
-
   for (let i in timeArray) {
     const timeOption = document.createElement('option');
     selectTime.appendChild(timeOption);
     timeOption.textContent = `${timeArray[i]}`;
-
   }
 }
-
-
+//this function to git data from form
 function handleSubmit(event) {
   event.preventDefault();
   new Service(
@@ -47,15 +36,12 @@ function handleSubmit(event) {
     document.getElementById('customerlocation').value,
     document.getElementById('service').value,
     document.getElementById('serviceTime').value);
-
+  //this line to set data in local storig
   localStorage.setItem('Service', JSON.stringify(Service.allService));
-
-  // renderOrderedServices();
-
   serviceForm.reset();
   renderOrderedServices();
 }
-
+// this function to get data from local storig and push in new array
 function getBackDataFromLocalstorage()
 {
   if(localStorage.Service)
@@ -66,29 +52,20 @@ function getBackDataFromLocalstorage()
     }
   }
 }
-
-
 getBackDataFromLocalstorage();
-
 populateForm();
-
+//EventListener buttom submit push all data to local storage
 let serviceForm = document.getElementById('serviceForm');
-
 serviceForm.addEventListener('submit', handleSubmit);
 
-
-
-
-
+//this function to git data from local storig and push it in table
 const ordersTable = document.getElementById('orderedSevices');
 function renderOrderedServices() {
   ordersTable.textContent='';
-
   if(localStorage.Service)
   {
     let temp=JSON.parse(localStorage.getItem('Service'));
     for (let index = 0; index < temp.length; index++) {
-      // new Service(temp[index].name,temp[index].location,temp[index].service,temp[index].time);
       const tableRow = document.createElement('tr');
       ordersTable.appendChild(tableRow);
       const deleteLink = document.createElement( 'button' );
@@ -96,8 +73,6 @@ function renderOrderedServices() {
       deleteLink.setAttribute( 'type', 'button' );
       deleteLink.setAttribute( 'value', 'delete' );
       deleteLink.setAttribute( 'onClick', 'deleteRow(this)' );
-      // deleteLink.id = `Order ${index}`;
-      // deleteLink.setAttribute( 'dlt', i );
       tableRow.appendChild(deleteLink);
       const customerNameData = document.createElement('td');
       tableRow.appendChild(customerNameData);
@@ -114,7 +89,6 @@ function renderOrderedServices() {
     }
   }
 }
-
 function deleteRow(r) {
   let i = r.parentNode.parentNode.rowIndex;
   ordersTable.deleteRow(i);
